f29a261e2815fdf7841dea11062c58d481b741b2
Francisco Samaniego <fjsamaniego1@outlook.com>
2024-04-03 18:41:58 -0600

    Started with class RunShop

--- MineCars.java
+++ MineCars.java
@@ -292,6 +292,14 @@ public class MineCars
     public class RunShop 
     {
         // attributes
+        public static void main(String[] args)
+        {
+
+        }
+
+        public static void runner1(){}
+        public static void runner2(){}
+        public static void runner3(){}
 
         // setters & getters
     }
