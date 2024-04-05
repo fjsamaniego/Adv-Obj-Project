d71de7b01c75a8b2bf393829ae228e7c0ba2e294
Francisco Samaniego <fjsamaniego1@outlook.com>
2024-04-04 21:17:08 -0700

    Updated Log.java and RunShop.java

--- RunShop.java
+++ RunShop.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 public class RunShop 
 {
     // attributes
     public static void main(String[] args)
     {
+        boolean inSystem = true;
+        Scanner scan = new Scanner(System.in);
+        while(inSystem)
+        {
+            System.out.println("Welcome to MineCars!");
+            System.out.println("Please choose one of the following:");
+            System.out.println("1. Login");
+            System.out.println("2. Exit");
+            System.out.println("Select either 1 or 2, please type it:");
+            int choice = scan.nextInt();
+            System.out.println();
+
+            if(choice == 1)
+            {
+                login();
+                inSystem = false;
+            }
+            else if(choice == 2)
+            {
+                System.exit(0);
+                inSystem = false;
+            }
+            else
+            {
+                System.out.println("Invalid, please select 1 or 2.");
+            }
+
+
+        }
+
 
     }
 
