90facaddc040be2eba1c30ca8cdf56c355999618
Cristina Alarcon <cristina@alarcon.me>
2024-04-07 16:18:12 -0600

    Entered the rest of the JavaDoc comments

--- Admin.java
+++ Admin.java
@@ -1,20 +1,35 @@
+/**
+ * Admin class extenteding person class
+ */
 public class Admin extends Person
 {   
     private int employeeID;
 
+    /**
+     * 
+     * @param username
+     * @param password
+     * @param employeeID
+     */
     public Admin(String username, String password, int employeeID) 
     {
         super(username, password);
         this.employeeID = employeeID;
     }
 
-    /** Setters  */
+    /**
+     * 
+     * @param newID
+     */
     public void setEmployeeID(int newID)
     {
         employeeID = newID;
     }
 
-
+    /**
+     * 
+     * @return
+     */
     public int getEmployeeID()
     {
         return employeeID;
