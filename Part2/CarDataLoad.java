5afc01af4188c184dd816870a6a454117e0b97f4
Francisco Samaniego <fjsamaniego1@outlook.com>
2024-04-13 16:46:10 -0600

    Modified CarDataLoad Implemented the methods from the interface Still needed to implement correctly dataUpdate Fix car constructor as well is needed

--- Part2/CarDataLoad.java
+++ Part2/CarDataLoad.java
@@ -10,15 +10,10 @@ import java.io.FileWriter;
  * CarDataLoad reads through car_data csv and find and sets Car attributes
  * to variables
  */
-public class CarDataLoad
+public class CarDataLoad implements DataLoad<Car>
 {
-    /**
-     * Loads car data from csv file
-     * 
-     * @param file pasth to the csv file
-     * @return list of cars loaded from file
-     */
-    public List<Car> loadCars(String file)
+    @Override
+    public List<Car> loadData(String file)
     {
         List<Car> cars = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(file)))
@@ -29,18 +24,19 @@ public class CarDataLoad
             {
                 String []carInformation = line.split(",");
 
-                int id = Integer.parseInt(carInformation[0]);
+                int capacity = Integer.parseInt(carInformation[0]);
                 String carType = carInformation[1];
-                String model = carInformation[2];
+                int carsAvailable = Integer.parseInt(carInformation[2]);
                 String condition = carInformation[3];
                 String color = carInformation[4];
-                int capacity = Integer.parseInt(carInformation[5]);
-                int mileage = Integer.parseInt(carInformation[6]);
-                String fuelType = carInformation[7];
+                int id = Integer.parseInt(carInformation[5]);
+                int year = Integer.parseInt(carInformation[6]);
+                double price = Double.parseDouble(carInformation[7]);
                 String transmission = carInformation[8];
                 String vin = carInformation[9];
-                double price = Double.parseDouble(carInformation[10]);
-                int carsAvailable = Integer.parseInt(carInformation[11]);
+                String fuelType = carInformation[10];
+                String model = carInformation[11];
+                String hasTurbo = carInformation[12];
 
                 Car car = new Car(id, carType ,model, condition,
                 color, capacity, mileage, fuelType, transmission, vin,
@@ -59,13 +55,8 @@ public class CarDataLoad
         return cars;
     }
 
-    /**
-     * Updates car data in file
-     * 
-     * @param cars list of cars to write to the file
-     * @param file the path to file
-     */
-    public void updateCars(List<Car> cars, String file)
+    @Override
+    public void updateData(List<Car> cars, String file)
     {
         try(BufferedWriter wr = new BufferedWriter(new FileWriter(file)))
         {
