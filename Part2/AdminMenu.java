6a2e63f5a84d756d58148cbab814128e5cfd0be5
Cristina Alarcon <cristina@alarcon.me>
2024-04-20 14:07:01 -0600

    add users method

--- Part2/AdminMenu.java
+++ Part2/AdminMenu.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
+/**
+ * 
+ */
 public class AdminMenu 
 {
     private List<Car> cars;
@@ -13,6 +16,13 @@ public class AdminMenu
     private Log logger = new Log();
     private int changesMade = 0;
 
+    /**
+     * 
+     * @param cars
+     * @param users
+     * @param userFile
+     * @param carFile
+     */
     public AdminMenu(List<Car> cars, List<User> users, String userFile, String carFile)
     {
         this.cars = cars;
@@ -22,6 +32,9 @@ public class AdminMenu
         this.scan = new Scanner(System.in);
     }
 
+    /**
+     * 
+     */
     public void MenuDisplay()
     {
         boolean stillLoggedIn = true;
@@ -69,6 +82,9 @@ public class AdminMenu
         }
     }
 
+    /**
+     * 
+     */
     private void addCar()
     {
         boolean addingCars = true;
@@ -125,6 +141,7 @@ public class AdminMenu
         return input;
     }
 
+
     private void getRevenue()
     {
 
@@ -135,10 +152,40 @@ public class AdminMenu
 
     }
 
+    /**
+     * 
+     */
     private void addMoreUsers()
     {
+        boolean addingUser = true;
+        while(addingUser)
+        {
+            ArrayList<String> userInformation = new ArrayList<>();
+            System.out.println("Please enter the information of the user. ");
+            System.out.println();
 
+            userInformation.add(userInput("Enter money available:"));
+            userInformation.add(userInput("Enter password:"));
+            userInformation.add(userInput("Enter user last name:"));
+            userInformation.add(userInput("Enter user ID:"));
+            userInformation.add(userInput("Enter number of cars purchased:"));
+            userInformation.add(userInput("Enter user first name:"));
+            userInformation.add(userInput("Enter username:"));
+            userInformation.add(userInput("Enter whether user has MinerCars membership: (TRUE/FALSE)"));
+
+            User newUser = UserDataLoad.loadData(userFile);
+            users.add(newUser);
+
+            System.out.println("Do you want to add another user (Y/N)");
+            String answer = scan.next();
+
+            if(answer.equalsIgnoreCase("N"))
+                addingUser = false;
+        }
+
+        new UserDataLoad().updateData(users, userFile);
     }
+    
 
     private void signOut()
     {
