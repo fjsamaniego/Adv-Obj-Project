998ae41f1b2087f594f8f9a4f9a41993d5461546
Francisco Samaniego <fjsamaniego1@outlook.com>
2024-04-22 16:56:58 -0600

    .

---
+++ Part3/CarFactory.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+
+/**
+ * File to create new car and set car fields from file to variables.
+ * This simplfies adding new cars by the admin
+ */
+public class CarFactory
+{
+    /**
+     * Sets a new Car object based on the provided car information.
+     * 
+     * @param carInformation an array containing car information based on car file order
+     * @return new car object
+     */
+    public static Car createCar(String[] carInformation)
+    {
+        int capacity = Integer.parseInt(carInformation[0]);
+        String carType = carInformation[1];
+        int carsAvailable = Integer.parseInt(carInformation[2]);
+        String condition = carInformation[3];
+        String color = carInformation[4];
+        int id = Integer.parseInt(carInformation[5]);
+        int year = Integer.parseInt(carInformation[6]);
+        double price = Double.parseDouble(carInformation[7]);
+        String transmission = carInformation[8];
+        String vin = carInformation[9];
+        String fuelType = carInformation[10];
+        String model = carInformation[11];
+        String hasTurbo = carInformation[12].isEmpty() ? "No" : carInformation[12];
+
+        switch (carType.toLowerCase())
+         {
+            case "sedan":
+                return new Sedan(id,carType,model,condition,color,capacity,
+                year,fuelType,transmission,vin,price,carsAvailable,hasTurbo);
+            case "suv":
+                return new SUV(id, carType, model, condition, color, capacity,
+                year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+            case "hatchback":
+                return new Hatchback(id, carType, model, condition, color, capacity,
+                year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+            case "pickup":
+                return new Hatchback(id, carType, model, condition, color, capacity,
+                year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+            default:
+                throw new IllegalArgumentException("Invalid car type");
+        }
+    }
+
+    /**
+     * Gets car object based on the provided car information.
+     * 
+     * @param carInformation An ArrayList containing car information
+     * @return The newly created Car object.
+     */
+    public static Car createCar(ArrayList<String> carInformation)
+    {
+        try
+        {
+            int capacity = Integer.parseInt(carInformation.get(0));
+            String carType = carInformation.get(1);
+            int carsAvailable = Integer.parseInt(carInformation.get(2));
+            String condition = carInformation.get(3);
+            String color = carInformation.get(4);
+            int id = Integer.parseInt(carInformation.get(5));
+            int year = Integer.parseInt(carInformation.get(6));
+            double price = Double.parseDouble(carInformation.get(7));
+            String transmission = carInformation.get(8);
+            String vin = carInformation.get(9);
+            String fuelType = carInformation.get(10);
+            String model = carInformation.get(11);
+            String hasTurbo = carInformation.get(12);
+
+            switch (carType.toLowerCase())
+            {
+                case "sedan":
+                    return new Sedan(id,carType,model,condition,color,capacity,
+                    year,fuelType,transmission,vin,price,carsAvailable,hasTurbo);
+                case "suv":
+                    return new SUV(id, carType, model, condition, color, capacity,
+                    year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+                case "hatchback":
+                    return new Hatchback(id, carType, model, condition, color, capacity,
+                    year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+                case "pickup":
+                    return new Hatchback(id, carType, model, condition, color, capacity,
+                    year, fuelType, transmission, vin, price, carsAvailable, hasTurbo);
+                default:
+                    throw new IllegalArgumentException("Invalid car type");
+        }
+        }
+        catch(NumberFormatException e)
+        {
+            throw new IllegalArgumentException("Error passing information", e);
+        }
+    }
+}
