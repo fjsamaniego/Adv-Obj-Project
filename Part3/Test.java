c2692616eb82ad66387813f61e995e20a697ba85
Cristina Alarcon <cristina@alarcon.me>
2024-04-30 23:52:57 -0600

    Junit

--- Part3/Test.java
+++ Part3/Test.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 
-public class UserMenuTest {
+public class Test {
     
     private List<Car> cars;
     private List<User> users;
